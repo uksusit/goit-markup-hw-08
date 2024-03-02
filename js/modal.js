1e4c5d96394e60cf4aeccaf4cd6f311eaa1e4f5c
uksusit <140089654+uksusit@users.noreply.github.com>
2024-03-02 10:57:11 +0200

    body no scroll

--- js/modal.js
+++ js/modal.js
@@ -8,7 +8,10 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
+  function toggleModal() {  
+    
+    document.body.classList.toggle("modal-open");
+
     refs.modal.classList.toggle("is-hidden");
   }
 })();
